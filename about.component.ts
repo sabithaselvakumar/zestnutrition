e9cab6090be0b6d7a17ba70a66e2e4535969cab8
Sabitha Selvakumar <112804881+sabithaselvakumar@users.noreply.github.com>
2024-06-05 14:28:34 +0530

    Add files via upload

---
+++ about.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 900,
+    autoplay: true,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  };
+}
